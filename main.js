e14cabacb5e41d275c91d073a16f166a2e11945b
Writaja <74670011+Writaja@users.noreply.github.com>
2021-07-13 19:56:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,74 +1,50 @@
-var canvas =new fabric.Canvas("myCanvas");
-var block_img_width=30;
-var block_img_height=30;
-var playar_X=10;
-var playar_Y=10;
-var playar_object="";
-var blocks="";
-function playar_update(){
-    fabric.Image.fromURL("player.png",function(Img){
-        playar_object=Img;
-        playar_object.scaleToWidth(150);
-        playar_object.scaleToHeight(140);
-playar_object.set({
-    top:playar_Y,
-    left:playar_X
 
-});
-canvas.add(playar_object);
-    });
+// Create canvas variable
+ block_y=1;
+ block_x=1;
 
+block_image_width = 350;
+block_image_height = 430;
+
+var block_image_object= "";
+
+function new_image(get_image)
+{
+	// to upload images
 }
-function new_img(get_img){
-    fabric.Image.fromURL(get_img,function(Img){
-        blocks=Img;
-        blocks.scaleToWidth( block_img_width);
-        blocks.scaleToHeight( block_img_height);
-        blocks.set({
-    top:playar_Y,
-    left:playar_X
-});
-canvas.add(blocks);
-    });
 
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '?') // add appropriate keycode
+	{
+		// upload red ranger
+	}
+	if(keyPressed == '71')
+	{
+		block_x = 200;
+		// upload green ranger
+	}
+	
+	if(keyPressed == '89')
+	{
+		block_x =350;
+		// upload yellow ranger
+	}
+	if(keyPressed == '80')
+	{
+		block_x = 600;
+		// upload pink ranger
+	}
+	if(keyPressed == '66')
+	{
+		block_x = 700;
+	// upload blue ranger
+	}
+	
 }
-window.addEventListener("keydown",my_keydown);
-function my_keydown(e) {
-keypressed=e.keyCode;
-console.log(keypressed);
-if(e.shiftKey==true && keypressed=="80"){
-    console.log("P and shift pressed togeather");
-    block_img_height=block_img_height+10;
-    block_img_width=block_img_width+10;
-    document.getElementById("current_width").innerHTML=block_img_width;
-    document.getElementById("current_height").innerHTML=block_img_height;
 
-}
-if(e.shiftKey==true && keypressed=="77"){
-    console.log("M and shift pressed togeather");
-    block_img_height=block_img_height-10;
-    block_img_width=block_img_width-10;
-    document.getElementById("current_width").innerHTML=block_img_width;
-    document.getElementById("current_height").innerHTML=block_img_height;}
-
-            if(keypressed=='82'){
-                new_img ("rr1.png");
-                console.log("r");
-                }
-                if(keypressed=='80'){
-                    new_img ("pr.png");
-                    console.log("p");
-                    }
-                    if(keypressed=='71'){
-                        new_img ("gr.png");
-                        console.log("g");
-                        }
-                        if(keypressed=='66'){
-                            new_img ("br.png");
-                            console.log("b");
-                            }  
-                           if(keypressed=='89'){
-                                    new_img ("yr.png");
-                                    console.log("y");
-                           }
-                        }
